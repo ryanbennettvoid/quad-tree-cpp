a7809625f664797a8abcc8f3c541b247fd616e35
Ryan Bennett <ryanbennettvoid@gmail.com>
2018-04-28 11:31:18 -0700

    init project setup

---
+++ src/Node.h
@@ -0,0 +1,9 @@
+#ifndef NODE_H
+#define NODE_H
+
+struct Node {
+  double x;
+  double y;
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include "./Node.h"
+
+int main() {
+
+  std::cout << "yo!";
+
+  return 0;
+}
